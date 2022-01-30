cc4d07eafccd586bdb0e70649fd53c6f6571763f
beonto <seanpark98@gmail.com>
2022-01-30 17:00:26 +0900

    Final migration from intranet to repo

---
+++ js/datetime.js
@@ -0,0 +1,21 @@
+/*JS file with a function to get current time for constant access by other functions*/
+
+
+let now;
+let year;
+let month;
+let day;
+let hour;
+let minute;
+let second;
+
+//Function to get the current date and time
+function currDateTime(){
+    now = new Date();
+    year = now.getFullYear;
+    month = now.getMonth()+ 1;
+    day = now.getDate();
+    hour = now.getHours();
+    minute = now.getMinutes();
+    second = now.getSeconds();
+}
